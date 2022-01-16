37f0a1e453204b46781458180d35df3b973d5c37
abdelrahmanMsaeed <eng.abdelrahman.M.saeed@gmail.com>
2022-01-16 22:40:33 +0200

    2

--- main.sh
+++ main.sh
@@ -9,14 +9,13 @@ select choice in 'Create DataBase' 'List DataBase' 'Connect To Database' 'Drop D
 do
 	case $REPLY in
 		1)
-			ls $1
+			create_DB
             ;;
 		2)
 			list_database
             ;;
 		3)
-			echo exiting
-            exit 0
+		    connect_DB
             ;;
         4)
             delete_database
@@ -38,6 +37,27 @@ function delete_database {
         echo "$REPLY has been Deleted!"
     fi
 }
+
+ function create_DB {
+
+    echo "enter your DB name"
+    read DB_Name
+    if [ -d DBS/$DB_Name ]
+    then 
+        echo "The DataBase called $DB_Name exist"
+        main
+    else
+        mkdir DBS/$DB_Name
+    fi
+}
+ function connect_DB {
+   echo "your available DB"
+   list_database
+   read Connect_DB
+   cd DBS/$Connect_DB
+
+}
+
 
 . ./list.sh
 
