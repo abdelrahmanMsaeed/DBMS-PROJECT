5c6c2b34ac6d9da4e041f1f10fc83e348202cae7
abdelrahmanMsaeed <eng.abdelrahman.M.saeed@gmail.com>
2022-01-18 00:51:26 +0200

    modify

--- table_funs.sh
+++ table_funs.sh
@@ -13,6 +13,7 @@ function touch_table () {
     touch $1
 }
 
+
 function cols_number () {
     echo "Enter your cols number ..."
     read  cols
@@ -24,7 +25,68 @@ function cols_number () {
     else
         echo "okay your DB will have $cols coulmns"
     fi
-    typeset -i hamada=cols
+    typeset -i cols_len=cols
+############################################################################ check type
+    typeset -i i=0
 
-}
+    while [ $i -lt $cols_len ]
+    do 
+        echo "type for the column number (($i+1))"
+
+        select choice in 'string' 'number' 
+        do
+            case $REPLY in
+                1)
+                    arr[i]=$REPLY;break
+                    ;;
+                2)
+                    arr[i]=$REPLY;break
+                    ;;
+                *)
+                    echo "Error select option 1 or 2";;
+            esac			
+        done
+        
+        array[i]=$REPLY
+        i=i+1
+
+    done
+    for value in ${array[*]}
+    do 
+        echo $value
+    done
+    }
+
+
+# function check_type () {
+#     typeset -i i=0
+
+#     while [ $i -lt $1 ]
+#     do 
+#         echo "type for the column number (($i+1))"
+
+#         select choice in 'string' 'number' 
+#         do
+#             case $REPLY in
+#                 1)
+#                     arr[i]=$REPLY;break
+#                     ;;
+#                 2)
+#                     arr[i]=$REPLY;break
+#                     ;;
+#                 *)
+#                     echo "Error select option 1 or 2";;
+#             esac			
+#         done
+        
+#         array[i]=$REPLY
+#         i=i+1
+
+#     done
+#     for value in ${array[*]}
+#     do 
+#         echo value
+#     done
+# }
+
 
