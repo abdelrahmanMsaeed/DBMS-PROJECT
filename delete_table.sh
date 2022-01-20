fd4ed950ae5bc661cad0b2d406129912699aa01d
mohamedmoselhy <mmeslhy110@gmail.com>
2022-01-20 20:19:40 +0200

    function delete added

--- delete_table.sh
+++ delete_table.sh
@@ -4,20 +4,27 @@ function delete_record () {
 
     echo "enter the pk for record to delete."
     read pk_del
+    echo ""
 
-    records_data=`awk -F";" '{print $1}'` $table_to_delte_from
-
+    records_data=`awk -F";" '{print $1}' $table_to_delte_from`
     echo $records_data
+    echo ""
     echo $pk_del
+
     for value in $records_data
     do
         if [ $value == $pk_del ]
         then
-            `sed '/$value/d' table_to_delte_from`
+            #sed -i "/$pk_del/d" $table_to_delte_from
+           # sed -i "/^$pk_del\b/Id" $table_to_delte_from
+            sed -i "/^$pk_del/d" $table_to_delte_from
+            #sed -i "0,/$pk_del/{/$pk_del/d;}" $table_to_delte_from
             echo "record deleted"
         else
             echo "no pk with this name"
         fi
     done
+
     
 }
+
